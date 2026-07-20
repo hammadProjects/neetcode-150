1f64bef4636440c86df45e0fe8b0abf6b21ddb20
Muhammad Hammad <hammadsarwar2200@gmail.com>
2026-07-20 18:14:03 +0500

    Time: 14 ms (42.46%) | Memory: 28.5 MB (55.32%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        vector<pair<int, string>> people;
+
+        for (int i = 0; i < names.size(); i++) {
+            people.push_back({heights[i], names[i]});
+        }
+
+        sort(people.begin(), people.end(), greater<pair<int, string>>());
+
+        vector<string> ans;
+        for (auto &p : people) {
+            ans.push_back(p.second);
+        }
+
+        return ans;
+    }
+};
